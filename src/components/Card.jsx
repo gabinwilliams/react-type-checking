07ef3d3ae321d1654681e55d33280db34d4e9d4d
gabinwilliams <scoutsnknives@gmail.com>
2021-06-27 11:27:28 +0200

    added default props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
+import PropTypes from 'prop-types';
 
-const CardExampleCard = ({name, age, desc, friends}) => (
+const CardExampleCard = ({name, age, desc, friends}) => {
+
+  // must provide default props or error of undefined will appear on 'isRequired' propTypes
+  Card.defaultProps = {
+    name: 'Thomas',
+    age: 22,
+    desc: 'default',
+    friends: 0,
+  }
+
+  // propTypes expected.  Will throw error is a different type is passed in
+  Card.propTypes = {
+    name: PropTypes.string.isRequired,
+    age: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+    desc: PropTypes.string.isRequired,
+    friends: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+    
+  }
+
+ return(
   <Card>
     <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
     <Card.Content>
@@ -14,12 +34,15 @@ const CardExampleCard = ({name, age, desc, friends}) => (
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
-      <a>
+      
         <Icon name='user' />
         {friends} Friends
-      </a>
+      
     </Card.Content>
   </Card>
 )
+ }
+
+
 
 export default CardExampleCard
